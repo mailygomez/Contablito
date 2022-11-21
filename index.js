ceec164dcf017a8a89ea6d523be6d7f8aeac9509
mailygomez <110416226+mailygomez@users.noreply.github.com>
2022-11-21 16:19:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+
+
+    button.addEventListener("click", function () {
+    
+    let descrip = document.getElementById("descripcion").value;
+let compra = document.getElementById("compra");
+let venta = document.getElementById("venta");
+let ivaBas= document.getElementById("basico");
+let ivaMin= document.getElementById("minimo");
+let ivaExento= document.getElementById("exento");
+let subTotal= document.getElementById("subTotal").value;
+
+
+console.log(holaaa)
+
+
+});
+
